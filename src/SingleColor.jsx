36ba48ee434f522973f9c2434b00226803d19979
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2022-09-05 20:15:25 +0530

    Add files via upload

---
+++ src/SingleColor.jsx
@@ -0,0 +1,43 @@
+import {useState,useEffect} from "react"
+import rgbToHex from "./utils.jsx"
+import toast, { Toaster } from 'react-hot-toast';
+
+
+const SingleColor =({rgb,weight,index,hexColor})=>{
+  const bcg =rgb.join(',');
+  console.log(bcg);
+
+const hexValue =`#${hexColor}`;
+  
+   const [alert,setAlert]=useState(false);
+  
+
+const notify = () => {
+  toast('Copied to ClipBoard');
+  setAlert(true);
+navigator.clipboard.writeText(hexValue);
+}
+ 
+  return ( <>
+    <article className={`color ${index>10 &&"color-light"}`} style={{backgroundColor: `rgb(${bcg})`}}
+    onClick={notify}
+      >
+      
+  <p className="percent-value">{weight}%</p>
+<p className="color-value">{hexValue}</p>
+  </article>
+    <Toaster
+      position="top-right" 
+     toastOptions={{
+    style: {
+      border: '1px solid #713200',
+      padding: '8px',
+      color: '#713200',
+    },
+  }}
+     />
+  </>
+          )
+}
+
+export default SingleColor;
